fab6c029c9a729c1ea244b6ac7a15cbb8392cd46
Semi Essessi <semi@cranium-software.co.uk>
2016-07-04 21:54:27 +0100

    clamp step functions

--- Maths/SmoothSteps.h
+++ Maths/SmoothSteps.h
@@ -8,22 +8,26 @@ namespace NNL
 
 static inline float SmoothStep( const float fX )
 {
-    return ( 3.0f - 2.0 * fX ) * fX * fX;
+    const float fClamped = ( fX > 1.0f ) ? 1.0f : ( ( fX < -0.0f ? 0.0f : fX ) );
+    return ( 3.0f - 2.0 * fClamped ) * fClamped * fClamped;
 }
 
 static inline float SmoothStepDerivative( const float fX )
 {
-    return ( 6.0f - 6.0 * fX ) * fX;
+    const float fClamped = ( fX > 1.0f ) ? 1.0f : ( ( fX < -0.0f ? 0.0f : fX ) );
+    return ( 6.0f - 6.0 * fClamped ) * fClamped;
 }
 
 static inline float SmootherStep( const float fX )
 {
-    return ( ( 6.0f * fX - 15.0f ) * fX + 10.0f ) * fX * fX * fX;
+    const float fClamped = ( fX > 1.0f ) ? 1.0f : ( ( fX < -0.0f ? 0.0f : fX ) );
+    return ( ( 6.0f * fClamped - 15.0f ) * fClamped + 10.0f ) * fClamped * fClamped * fClamped;
 }
 
 static inline float SmootherStepDerivative( const float fX )
 {
-    return ( ( 30.0f * fX - 60.0f ) * fX + 30.0f ) * fX * fX;
+    const float fClamped = ( fX > 1.0f ) ? 1.0f : ( ( fX < -0.0f ? 0.0f : fX ) );
+    return ( ( 30.0f * fClamped - 60.0f ) * fClamped + 30.0f ) * fClamped * fClamped;
 }
 
 }
