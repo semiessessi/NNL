d929ea4d36fc095041729b945302f9cf76f6cc7c
semi.essessi <semi.essessi>
2016-07-05 15:24:23 +0100

    some changes i had lying around

--- Network/AnalyticBackpropagatingNetwork.h
+++ Network/AnalyticBackpropagatingNetwork.h
@@ -1,5 +1,12 @@
-#ifndef SIGMOID_NETWORK
-#define SIGMOID_NETWORK
+// Copyright (c) 2015 Cranium Software
+
+#ifndef ANALYTIC_BACKPROPAGATING_NETWORK
+#define ANALYTIC_BACKPROPAGATING_NETWORK
+
+#include "Maths/Random.h"
+
+namespace NNL
+{
 
 template< int iLayerCount, int iInputCount >
 class AnalyticBackpropagatingNetwork
@@ -34,6 +41,16 @@ public:
     const int GetLayerCount() const { return iLayerCount; }
     float GetOutput( const int iIndex ) const { return mapfPotentials[ iLayerCount - 1 ][ iIndex ]; }
 
+    void Load( const char* const /*szPath*/ )
+    {
+        // SE - TODO: ...
+    }
+
+    void Save( const char* const /*szPath*/ )
+    {
+        // SE - TODO: ...
+    }
+
     float* FeedForward( const float* const pfInputs, float ( * const pfnSummingFunction )( const float ) )
     {
         for( int i = 0; i < iInputCount; ++i )
@@ -54,9 +71,8 @@ public:
 
     void BackPropagate( const float* const pfCorrectOutputs, const float fLearningRate, float( *const pfnDerivativeSummingFunction )( const float ) )
     {
-        // clear the error signals
+        // reset all of the error signals to zero
         const int iLastLayer = iLayerCount - 1;
-        const int iLastLayerInputCount = GetInputCount( iLastLayer );
 
         for( int i = 0; i < iLayerCount; ++i )
         {
@@ -66,48 +82,17 @@ public:
             }
         }
 
-        // start from the last layer...
+        // fill out the error signals into the last layer...
         for( int i = 0; i < GetNeuronCount( iLastLayer ); ++i )
         {
             const float fDiff = pfCorrectOutputs[ i ] - mapfPotentials[ iLastLayer ][ i ];
-            const float fOriginalSum = mapfSums[ iLastLayer ][ i ];
-            const float fDerivative = pfnDerivativeSummingFunction( fOriginalSum );
-            mapfErrorSignals[ iLastLayer ][ i ] = fDiff * fDerivative;
-            for( int j = 0; j < iLastLayerInputCount; ++j )
-            {
-                // dP/dw[i] = dP/du du/dw[ i ] = S'( w[ i ] x[ i ] + c ) x[ i ]
-                mapfWeights[ iLastLayer ][ j + i * iInputCount ] += fLearningRate * mapfErrorSignals[ iLastLayer ][ i ] * mapfPotentials[ iLastLayer - 1 ][ j ];
-            }
-
-            // dP/db = dP/du du/db = S'( b + c )
-            mapfBiases[ iLastLayer ][ i ] += fLearningRate * mapfErrorSignals[ iLastLayer ][ i ];
-
-            for( int j = 0; j < iLastLayerInputCount; ++j )
-            {
-                mapfErrorSignals[ iLastLayer - 1 ][ j ] += mapfErrorSignals[ iLastLayer ][ i ] * mapfWeights[ iLastLayer ][ j + i * iInputCount ];
-            }
+            mapfErrorSignals[ iLastLayer ][ i ] = fDiff;
         }
-        // then go back through the rest
-        for( int i = iLastLayer - 1; i > 0; --i )
+        
+        // then go back through the layers propagating it
+        for( int i = iLastLayer; i >= 0; --i )
         {
             BackPropagateLayer( i, fLearningRate, pfnDerivativeSummingFunction );
-        }
-
-        // then finally the first layer.
-        for( int i = 0; i < GetNeuronCount( 0 ); ++i )
-        {
-            const float fDiff = pfCorrectOutputs[ i ] - mapfPotentials[ 0 ][ i ];
-            const float fOriginalSum = mapfSums[ 0 ][ i ];
-            const float fDerivative = pfnDerivativeSummingFunction( fOriginalSum );
-            mapfErrorSignals[ 0 ][ i ] = fDiff * fDerivative;
-            for( int j = 0; j < GetInputCount( 0 ); ++j )
-            {
-                // dP/dw[i] = dP/du du/dw[ i ] = S'( w[ i ] x[ i ] + c ) x[ i ]
-                mapfWeights[ 0 ][ j + i * iInputCount ] += fLearningRate * mapfErrorSignals[ 0 ][ i ] * mafInputs[ j ];
-            }
-
-            // dP/db = dP/du du/db = S'( b + c )
-            mapfBiases[ 0 ][ i ] += fLearningRate * mapfErrorSignals[ iLastLayer ][ i ];
         }
     }
 
@@ -115,37 +100,19 @@ protected:
 
     void FeedForwardLayer( const int iLayer, float( *const pfnSummingFunction )( const float ) )
     {
-        if( iLayer == 0 )
+        for( int i = 0; i < GetNeuronCount( iLayer ); ++i )
         {
-            for( int j = 0; j < GetNeuronCount( 0 ); ++j )
+            // take each input, multiply it by a weight and accumulate it into a sum
+            mapfSums[ iLayer ][ i ] = 0.0f;
+            const int iLayerInputCount = GetInputCount( iLayer );
+            for( int j = 0; j < iLayerInputCount; ++j )
             {
-                // take each input, multiply it by a weight and accumulate it into a sum
-                mapfSums[ iLayer ][ j ] = 0.0f;
-                const int iLayerInputCount = GetInputCount( 0 );
-                for( int k = 0; k < iLayerInputCount; ++k )
-                {
-                    const float fInput = mafInputs[ k ];
-                    mapfSums[ iLayer ][ j ] += mapfWeights[ iLayer ][ k + j * iLayerInputCount ] * fInput;
-                }
-
-                mapfPotentials[ iLayer ][ j ] = pfnSummingFunction( mapfSums[ iLayer ][ j ] );
+                // SE - NOTE: hopefully this conditional is optimised out
+                const float fInput = ( iLayer == 0 ) ? mafInputs[ j ] : mapfPotentials[ iLayer - 1 ][ j ];
+                mapfSums[ iLayer ][ i ] += mapfWeights[ iLayer ][ j + i * iLayerInputCount ] * fInput;
             }
-        }
-        else
-        {
-            for( int j = 0; j < GetNeuronCount( 0 ); ++j )
-            {
-                // take each input, multiply it by a weight and accumulate it into a sum
-                mapfSums[ iLayer ][ j ] = 0.0f;
-                const int iLayerInputCount = GetInputCount( 0 );
-                for( int k = 0; k < iLayerInputCount; ++k )
-                {
-                    const float fInput = mapfPotentials[ iLayer - 1 ][ k ];
-                    mapfSums[ iLayer ][ j ] += mapfWeights[ iLayer ][ k + j * iLayerInputCount ] * fInput;
-                }
 
-                mapfPotentials[ iLayer ][ j ] = pfnSummingFunction( mapfSums[ iLayer ][ j ] );
-            }
+            mapfPotentials[ iLayer ][ i ] = pfnSummingFunction( mapfSums[ iLayer ][ i ] );
         }
     }
 
@@ -153,24 +120,28 @@ protected:
     {
         for( int i = 0; i < GetNeuronCount( iLayer ); ++i )
         {
-            const float fDiff = mapfErrorSignals[ iLayer + 1 ][ i ];
+            const float fDiff = mapfErrorSignals[ iLayer ][ i ];
             const float fOriginalSum = mapfSums[ iLayer ][ i ];
             const float fDerivative = pfnDerivativeSummingFunction( fOriginalSum );
             const int iLayerInputCount = GetInputCount( iLayer );
-            mapfErrorSignals[ iLayer ][ i ] = fDiff * fDerivative;
+            const float fErrorSignal = fDiff * fDerivative;
             for( int j = 0; j < iLayerInputCount; ++j )
             {
                 // dP/dw[i] = dP/du du/dw[ i ] = S'( w[ i ] x[ i ] + c ) x[ i ]
-                mapfWeights[ iLayer ][ j + i * iLayerInputCount ] += fLearningRate * mapfErrorSignals[ iLayer ][ i ] * mapfPotentials[ iLayer - 1 ][ j ];
+                // SE - NOTE: hopefully this conditional is optimised out
+                const float fInputPotential = ( iLayer == 0 ) ? mafInputs[ j ] : mapfPotentials[ iLayer - 1 ][ j ];
+                mapfWeights[ iLayer ][ j + i * iLayerInputCount ] += fLearningRate * fErrorSignal * fInputPotential;
+
+                // SE - NOTE: hopefully this conditional is optimised out
+                if( iLayer != 0 )
+                {
+                    // accumulate some error signal
+                    mapfErrorSignals[ iLayer - 1 ][ j ] += fErrorSignal * mapfWeights[ iLayer ][ j + i * iLayerInputCount ];
+                }
             }
 
             // dP/db = dP/du du/db = S'( b + c )
-            mapfBiases[ iLayer ][ i ] += fLearningRate * mapfErrorSignals[ iLayer ][ i ];
-
-            for( int j = 0; j < iLayerInputCount; ++j )
-            {
-                mapfErrorSignals[ iLayer - 1 ][ j ] += mapfErrorSignals[ iLayer ][ i ] * mapfWeights[ iLayer ][ j + i * iLayerInputCount ];
-            }
+            mapfBiases[ iLayer ][ i ] += fLearningRate * fErrorSignal;
         }
     }
 
@@ -187,9 +158,9 @@ protected:
 
     int GetWeightCount( const int iLayer ) const
     {
-        const int iInputCount = GetInputCount( iLayer );
+        const int iLayerInputCount = GetInputCount( iLayer );
         const int iNeuronCount = GetNeuronCount( iLayer );
-        return iInputCount * iNeuronCount;
+        return iLayerInputCount * iNeuronCount;
     }
 
     int GetNeuronCount( const int iLayer ) const
@@ -206,8 +177,20 @@ protected:
     {
         for( int i = 0; i < iLayerCount; ++i )
         {
-            mapfWeights[ i ] = new float[ GetWeightCount( i ) ];
-            mapfBiases[ i ] = new float[ GetNeuronCount( i ) ];
+            const int iWeightCount = GetWeightCount( i );
+            const int iNeuronCount = GetNeuronCount( i );
+            mapfWeights[ i ] = new float[ iWeightCount ];
+            mapfBiases[ i ] = new float[ iNeuronCount ];
+            // initialise values. very important!
+            for( int j = 0; j < iWeightCount; ++j )
+            {
+                mapfWeights[ i ][ j ] = WeakRandom();
+            }
+
+            for( int j = 0; j < iNeuronCount; ++j )
+            {
+                mapfBiases[ i ][ j ] = WeakRandom();
+            }
         }
     }
 
@@ -236,5 +219,7 @@ protected:
     float* mapfPotentials[ iLayerCount ];
     float* mapfErrorSignals[ iLayerCount ];
 };
+
+}
 
 #endif
